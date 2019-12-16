52c0c26b8e6898bf028dfcf4db3edaeefdda7d4a
MarionMcG <g00364792@gmit.ie>
2019-12-16 21:41:08 +0000

    Finished all questions

--- recursive.py
+++ recursive.py
@@ -1,4 +1,3 @@
-
 #Sum of an array (Recursive)
 def sum_(arr):
     if len(arr)==0: 
@@ -13,30 +12,31 @@ def product(arr):
     else:
         return arr[0]*(product(arr[1:])) 
 
+
 arr2 = []
-#Remove Odd numbers (Recursive but only works with an intenal print statement)
+#Remove Odd numbers
 def odd_out(arr):  
     if len(arr) == 0:
-        print(arr2)
-        #return arr2 
+
+        return arr2 
     else:
         if arr[0] % 2 == 0:
             arr2.append(arr[0]) 
-            odd_out(arr[1:])
+            return odd_out(arr[1:])
         else:
-            odd_out(arr[1:])
+            return odd_out(arr[1:])
 
 #Remove Even numbers
 def even_out(arr):
     if len(arr) == 0:
-        print(arr2)
         return arr2  
     else:
         if arr[0]%2!=0:
             arr2.append(arr[0]) 
-            even_out(arr[1:])
+            return even_out(arr[1:])
         else:
-            even_out(arr[1:])
+            return even_out(arr[1:])
+
 
 new_arr = []
 def replace_char(arr):
@@ -45,12 +45,9 @@ def replace_char(arr):
     else:
         new_arr.append(arr[0].replace('o', '*'))
         replace_char(arr[1:])
-
     return new_arr
 
-
 def searc_ndex(arr, inte, loc = 0):
-
     while  len(arr) > 0:
         if inte not in arr:
             return -1
@@ -59,15 +56,13 @@ def searc_ndex(arr, inte, loc = 0):
         else:
             return searc_ndex(arr, inte, loc+1)
 
-
-
 def sum_digits(inte, sum_=0):
     if inte == 0:
         return sum_
     else:
-        digit = inte % 10
-        sum_ += digit
-        inte //= 10
+        digit = inte % 10 #gives value of digit in the tens position
+        sum_ += digit #add to sum
+        inte //= 10 #inte is now the value in the hundreds position
         return sum_digits(inte, sum_)
      
 def print_array(arr):
@@ -77,44 +72,41 @@ def print_array(arr):
         print(arr[0])
         print_array(arr[1:])
 
-
-
-
-
-min_=0
 def min_value(arr):
     if len(arr)==0:
         return 0
     if len(arr) == 1:
         return arr[0]
     else:
-        min_ = arr[0]
-        next_ = arr[1]
-        if next_ < min_:
-            min_ = next_
-            return (min_value(arr[1:]), min_)
+        if arr[1] < arr[0]:
+            return (min_value(arr[1:]))
         else:
-            return (min_value(arr[1:]), min_)
+            arr[1]=arr[0]
+            return (min_value(arr[1:]))
 
             
-a = [1, 2, 4, 5, 3]
+a = [1, 2, 4, -36, 5, 3]
 print('Array A: ', a)
 b = ['hello', 'world']
 print('Array B: ', b)
+print()
 print('Sum of Array A: ',sum_(a))
 print('Product of Array A: ', product(a))
-print('Array A without odd numbers')
-odd_out(a)
+print()
+print('Array A without odd numbers', odd_out(a))
 
 del arr2
 arr2=[]
 
-print('Array A without even numbers')
-even_out(a)
-
+print('Array A without even numbers', even_out(a))
+print()
 print('Array B, replacing char o with * , ',replace_char(b))
+print()
 print('In Array A the number 2 is at index ',searc_ndex(a, 2))
-
-print('The sum of the digits of 78 is ',sum_digits(78))
+print()
+print('The sum of the digits of 78 is ', sum_digits(78))
+print()
+print('Printing every value in Array A, each on a new line')
 print_array(a)
-print(min_value(a))
+print()
+print('Minimum value in Array A: ',min_value(a))
